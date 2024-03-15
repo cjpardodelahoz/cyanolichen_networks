04708c0b593eeff0632af3be393fcb9a7a37b624
cjpardodelahoz <cjpardodelahoz@gmail.com>
2024-03-14 21:31:18 -0400

    added the site voucher data and corrected the PA ranges

--- scripts/tidy_data/assign_lichen_dna_codes.R
+++ scripts/tidy_data/assign_lichen_dna_codes.R
@@ -6,10 +6,15 @@ library(tidyverse)
 # Load table with ID ranges
 pa_ranges <- read_csv("data/tables/pa_range_tidy.csv") %>%
   select(1:5)
+# Load site voucher data
+site_voucher <- read_csv("data/tables/site_voucher.csv")
 # Expand ranges into specimen id table
 lichen_dna_codes <- pa_ranges %>%
   rowwise() %>%
   mutate(dna_code = list(seq(first, last))) %>%
   unnest(dna_code) %>%
   select(-first, -last) %>%
-  mutate(dna_code = paste("PA", dna_code, sep = ""))
+  mutate(dna_code = paste("PA", dna_code, sep = "")) %>%
+  left_join(site_voucher, by = "site")
+# Save table with lichen DNA codes
+write_csv(lichen_dna_codes, "documents/tables/lichen_dna_codes.csv")
