60481db61c58071c67b1e97f14aad70a75422527
cjpardodelahoz <cjpardodelahoz@gmail.com>
2024-08-08 10:27:35 -0400

    sorted and relabeled reads from pacbio_env first batch

---
+++ scripts/environmental_sequencing/pacbio_env/sort_and_label_reads_batch1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --array=2-385
+#SBATCH --mem-per-cpu=32G  # adjust as needed
+#SBATCH -c 1 # number of threads per process
+#SBATCH --output=log/environmental_sequencing/pacbio_env/sort_and_label_reads_batch1.out
+#SBATCH --error=log/environmental_sequencing/pacbio_env/sort_and_label_reads_batch1.err
+#SBATCH --partition=scavenger
+
+# Variable with the barcode combination
+barcode=$(sed -n ${SLURM_ARRAY_TASK_ID}p misc_files/environmental_sequencing/pacbio_env/revio_order_10070_barcode.csv | cut -d, -f1)
+# Variable witht he sample name
+sample=$(sed -n ${SLURM_ARRAY_TASK_ID}p misc_files/environmental_sequencing/pacbio_env/revio_order_10070_barcode.csv | cut -d, -f2)
+# Make a directory for the copy of the reads
+mkdir -p analyses/reads/pacbio_env
+# Make a working copy of the reads with the sample name
+cp data/reads/0000000075/outputs/fastx_files/*${barcode}*fastq.gz analyses/reads/pacbio_env/${sample}.fastq.gz
