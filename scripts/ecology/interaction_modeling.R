3873447bdda38f8e599289f0ce09ac58cc7714bf
cjpardodelahoz <cjpardodelahoz@gmail.com>
2024-12-08 19:25:02 -0500

    setup to fit models to all pairs, get summary table, rank plot, and likelihood gain plot

--- scripts/ecology/interaction_modeling.R
+++ scripts/ecology/interaction_modeling.R
@@ -10,6 +10,7 @@ source("scripts/ecology/gravel2019_functions/interactions_models.r")
 source("scripts/ecology/gravel2019_functions/get_LL.r")
 source("scripts/ecology/gravel2019_functions/get_probs.r")
 source("scripts/ecology/gravel2019_functions/fit_models.r")
+source("scripts/ecology/gravel2019_functions/fit_all_pairs.r")
 
 
 ##### PREP REGIONAL DATA FOR MODEL FITTING #####
@@ -117,6 +118,92 @@ pairs_ID <- as.factor(paste(DF$IDi,DF$IDj))
 DF_split <- split(DF,pairs_ID)
 
 
+##### FIT MODELS FOR ALL PAIRS #####
+
+DF_split <- DF_split[c(806, 913)]
+# Specify the environmental variables
+Enames = c("T","T2","PP","PP2")
+
+# Fit all models to all pairs
+fit_result <- fit_all_pairs(DF_split, Enames)
+
+# Summarize the results across all pairs
+full_fit_summary <- fit_result %>%
+    group_by(model) %>%
+    summarize(LL = sum(LL),
+              npars = sum(npars),
+              AIC = -2 * LL + 2 * npars)
+
+# Write the full fit summary to a CSV file
+write_csv(fit_summary, "fit_summary.csv")
+
+# Rank models by AIC
+model_ranks <- fit_result %>%
+    group_by(mycobiont, nostoc) %>%
+    mutate(rank = rank(AIC))
+
+# Calculate the percent likelihood gain for the best model compared to the second-best model
+likelihood_gain <- fit_result %>%
+    group_by(mycobiont, nostoc) %>%
+    mutate(rank = rank(AIC)) %>%
+    filter(rank %in% c(1, 2 ,6)) %>%
+    group_by(mycobiont, nostoc) %>%
+    summarize(percent_likelihood_gain = -(LL[rank == 2] - LL[rank == 1]) / (LL[rank == 6] - LL[rank == 1])*100)
+    
+
+##### PLOT MODEL FITTING RESULTS #####
+
+# Define a custom theme for plots
+custom_theme <- theme(
+    panel.grid = element_blank(),
+    panel.background = element_blank(),
+    panel.border = element_rect(color = "black", fill = NA, linewidth = 0.75), 
+    axis.text = element_text(color = "black", size = 12),
+    legend.text = element_text(color = "black", size = 12),
+    legend.title = element_text(color = "black", size = 12)
+    )
+
+# Custom colors for each model
+model_colors <- c("C0_L2" = "red", 
+                    "C1_L2" = "blue", 
+                    "C2_L0" = "green", 
+                    "C2_L1" = "purple", 
+                    "C2_L2" = "orange", 
+                    "C3_L2" = "yellow")
+
+# Custom labels for the legend
+model_labels <- c("C0_L2" = "Model C0_L2",
+                   "C1_L2" = "Model C1_L2",
+                   "C2_L0" = "Model C2_L0",
+                   "C2_L1" = "Model C2_L1",
+                   "C2_L2" = "Model C2_L2",
+                   "C3_L2" = "Model C3_L2")
+
+# Plot proportion of times each model was in each rank
+model_ranks %>%
+    ggplot(aes(x = rank, fill = model)) +
+    geom_bar(position = "fill") +
+    labs(x = "Rank", y = "Proportion") +
+    scale_x_continuous(breaks = c(1, 2, 3, 4, 5, 6)) +
+    scale_fill_manual(values = model_colors, labels = model_labels) +
+    guides(fill = guide_legend(title = "Model")) +
+    custom_theme
+
+# Save the plot as a PDF
+ggsave("fig.pdf", width = 6, height = 4)
+
+
+# Hiistogram of the percent likelihood gain
+likelihood_gain %>%
+    ggplot(aes(x = percent_likelihood_gain)) +
+    geom_histogram(binwidth = 5, fill = "gray70") +
+    labs(x = "% likelihood gain model 2 vs model 1", y = "Frequency") +
+    custom_theme
+
+# Save the plot as a PDF
+ggsave("fig2.pdf", width = 6, height = 4)
+
+
 ##### EXAMPLE PAIR #####
 
 # Find the nb of links per pair
@@ -134,19 +221,22 @@ for(x in 1:length(DF_split)) {
 cbind(nL,nXi,nXj,nXij,nL,nL/nXij)[nL/nXij<0.7 & nL > 10,]
 
 # Subset the data
-pair_index = which(nL == 25 & nXij == 43) 
+pair_index = which(nL == 36 & nXij == 68) 
 data = DF_split[[pair_index]]
-data$E = data.frame(T = data$E$T/12, PP = data$E$PP/1000, T2 = data$E$T2/12^2, PP2 = data$E$PP2/1000^2)
+#data$E = data.frame(T = data$E$T/12, PP = data$E$PP/1000, T2 = data$E$T2/12^2, PP2 = data$E$PP2/1000^2)
 sum(data$Lij)
 sum(data$Xij)
 
+# Specify the environmental variables
+Enames = c("T","T2","PP","PP2")
+
 # Pick the model
-models_C2_L0 = fit_models(data, selection = FALSE, funC = C2, funL = L0)
-models_C2_L1 = fit_models(data, selection = FALSE, funC = C2, funL = L1)
-models_C2_L2 = fit_models(data, selection = FALSE, funC = C2, funL = L2)
-models_C0_L2 = fit_models(data, selection = FALSE, funC = C0, funL = L2)
-models_C1_L2 = fit_models(data, selection = FALSE, funC = C1, funL = L2)
-models_C3_L2 = fit_models(data, selection = FALSE, funC = C3, funL = L2)
+models_C2_L0 = fit_models(data, selection = FALSE, funC = C2, funL = L0, Enames)
+models_C2_L1 = fit_models(data, selection = FALSE, funC = C2, funL = L1, Enames)
+models_C2_L2 = fit_models(data, selection = FALSE, funC = C2, funL = L2, Enames)
+models_C0_L2 = fit_models(data, selection = FALSE, funC = C0, funL = L2, Enames)
+models_C1_L2 = fit_models(data, selection = FALSE, funC = C1, funL = L2, Enames)
+models_C3_L2 = fit_models(data, selection = FALSE, funC = C3, funL = L2, Enames)
 
 # Compute the LL
 LL_C2_L0 = get_LL(models_C2_L0, data)
