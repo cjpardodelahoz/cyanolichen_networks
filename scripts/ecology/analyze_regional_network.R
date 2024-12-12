fd4e12b04dbeb41c429330158d09e82be4a53cfd
cjpardodelahoz <cjpardodelahoz@gmail.com>
2024-12-12 14:47:08 -0500

    separating struture quantification from plotting for efficiency

--- scripts/ecology/analyze_regional_network.R
+++ scripts/ecology/analyze_regional_network.R
@@ -128,36 +128,38 @@ quantify_network_topology <- function(interaction_matrix, network_name) {
     }    
     return(network_metrics)
 }
+
+
 ##### ID DATA TO INTERACTION MATRICES #####
 
-# Load ABMI dataset from Pardo-De la Hoz et al. (2024)-Data S1C
-abmi_data <- read_csv("data/tables/nostoc_datas1c_includes_ined.csv") %>% 
-    select(1:Collector) %>%
+# Load ABMI dataset
+abmi_data <- read_csv("data/tables/abmi_id_data.csv") #%>% 
+    #select(1:Collector) %>%
     # Rename column names as lower case and replace spaces with underscores
-    rename_all(tolower) %>%
-    rename_all(~str_replace_all(., " ", "_")) %>%
+    #rename_all(tolower) %>%
+    #rename_all(~str_replace_all(., " ", "_")) %>%
     # When species complex is "3.10a", replace section with "3.10"
-    mutate(section = case_when(
-        species_complex == "3.10a" ~ "3.10",
-        .default = section
-    )) %>%
+    #mutate(section = case_when(
+    #    species_complex == "3.10a" ~ "3.10",
+    #    .default = section
+    #)) %>%
     # If the section column is not NA, append the string "section" to the section column
-    mutate(section = case_when(
-        !is.na(section) ~ paste0("section_", section),
-        .default = section
-    )) %>%
+    #mutate(section = case_when(
+    #    !is.na(section) ~ paste0("section_", section),
+    #    .default = section
+    #)) %>%
     # If the species_complex column is not NA, append the string "species_complex" to the species_complex column
-    mutate(species_complex = case_when(
-        !is.na(species_complex) ~ paste0("species_complex_", species_complex),
-        .default = species_complex
-    )) %>%
+    #mutate(species_complex = case_when(
+    #    !is.na(species_complex) ~ paste0("species_complex_", species_complex),
+    #    .default = species_complex
+    #)) %>%
     # Coalesce the columns phylogroup and species complex into a new column called "nostoc_otu"
-    mutate(nostoc_otu = coalesce(phylogroup, species_complex))
+    #mutate(nostoc_otu = coalesce(phylogroup, species_complex))
 
 # Get full interaction matrix including all species
-abmi_matrix_all <- abmi_data %>%
-    # Filter the data to include only rows where mycobiont_molecular_id is not NA
-    filter(!is.na(mycobiont_molecular_id)) %>%
+abmi_matrix_full <- abmi_data %>%
+    # Filter to records with IDs for both mycobiont and nostoc
+    filter(!is.na(mycobiont_molecular_id), !is.na(nostoc_otu)) %>%
     # Select only the columns mycobiont_molecular_id and nostoc_otu
     select(mycobiont_molecular_id, nostoc_otu) %>%
     # Count the number of times each pair of nostoc_otu and mycobiont_molecular_id occurs
@@ -182,140 +184,29 @@ abmi_matrix_peltigera <- abmi_data %>%
     column_to_rownames("mycobiont_molecular_id") %>%
     as.matrix()
 
+# Save the interaction matrix objects to a file
+if(!dir.exists("analyses/ecology")) {
+    dir.create("analyses/ecology", recursive = TRUE)
+}
+save(abmi_matrix_full, file = "analyses/ecology/full_interaction_matrix.RData")
+save(abmi_matrix_peltigera, file = "analyses/ecology/peltigera_interaction_matrix.RData")
+
+
 ##### REGIONAL NETWORK TOPOLOGY ANALYSES #####
 
 # Compute metrics for the Peltigera network and the full network
 peltigera_network_metrics <- quantify_network_topology(abmi_matrix_peltigera, "peltigera")
-all_network_metrics <- quantify_network_topology(abmi_matrix_all, "all")
+full_network_metrics <- quantify_network_topology(abmi_matrix_full, "full")
+
 # Merge the network metrics into a single table
-regional_network_metrics <- bind_rows(peltigera_network_metrics, all_network_metrics)
+regional_network_metrics <- bind_rows(peltigera_network_metrics, full_network_metrics)
+
+# Save the network metric objects to a file
+if(!dir.exists("analyses/ecology")) {
+    dir.create("analyses/ecology", recursive = TRUE)
+}
+save(peltigera_network_metrics, file = "analyses/ecology/peltigera_regional_network_metrics.RData")
+save(full_network_metrics, file = "analyses/ecology/full_regional_network_metrics.RData")
+
 # Save the network metrics table to a CSV file
 write_csv(network_metrics, "documents/tables/regional_network_metrics.csv")
-
-##### PLOT THE NETWORKS HIGHLIGHTING COMPOUND STRUCTURE #####
-
-# Get module objects for the networks
-peltigera_modules <- bipartite::metaComputeModules(abmi_matrix_peltigera, N = 5, method = "Beckett")
-all_modules <- bipartite::metaComputeModules(abmi_matrix_all, N = 5, method = "Beckett")
-# Get module partitions 
-peltigera_module_partitions <- bipartite::module2constraints(peltigera_module_partitions)
-all_module_partitions <- bipartite::module2constraints(all_module_partitions)
-# Get row and column partitions
-peltigera_row_partitions <- peltigera_module_partitions[1:nrow(abmi_matrix_peltigera)]
-peltigera_col_partitions <- peltigera_module_partitions[(nrow(abmi_matrix_peltigera)+1):(nrow(abmi_matrix_peltigera)+ncol(abmi_matrix_peltigera))]
-all_row_partitions <- all_module_partitions[1:nrow(abmi_matrix_all)]
-all_col_partitions <- all_module_partitions[(nrow(abmi_matrix_all)+1):(nrow(abmi_matrix_all)+ncol(abmi_matrix_all))]
-# Sort matrices in compound topology
-peltigera_matrix_compound <- bipartite::sortmatrix(abmi_matrix_peltigera, 
-    topology = "compound", sort_by = "weights", 
-    row_partitions = peltigera_row_partitions, col_partitions = peltigera_col_partitions,
-    mod_similarity = TRUE)
-# Get row and column partitions for the sorted matrix
-peltigera_x_partitions <- peltigera_matrix_compound$row_partitions
-peltigera_y_partitions <- rev(peltigera_matrix_compound$col_partitions)
-
-# Reshape matrix data for ggplot
-abmi_peltigera_matrix_df <- abmi_matrix_peltigera %>%
-    as.data.frame() %>%
-    rownames_to_column(var = "mycobiont_molecular_id") %>%
-    pivot_longer(cols = -mycobiont_molecular_id, names_to = "nostoc_otu", values_to = "interaction_frequency") %>%
-    left_join(distinct(abmi_data, mycobiont_molecular_id, peltigera_section), by = "mycobiont_molecular_id") %>%
-    left_join(distinct(abmi_data, nostoc_otu, section), by = "nostoc_otu") %>%
-    select(mycobiont_molecular_id, nostoc_otu, interaction_frequency, section,peltigera_section)
-# Set order for Nostoc section legend
-abmi_peltigera_matrix_df$section <- factor(abmi_peltigera_matrix_df$section, 
-                                        levels = c("section_2.4", "section_3.1", "section_3.2", "section_3.4", "section_3.5", "section_3.6",
-                                            "section_3.7", "section_3.9", "section_3.10", "section_3.11", "section_3.12"))
-# Convert Rows and Columns to factors with levels matching the orded of the sorted matrix
-abmi_peltigera_matrix_df$mycobiont_molecular_id <- factor(abmi_peltigera_matrix_df$mycobiont_molecular_id, levels = rownames(peltigera_matrix_compound$matrix))
-abmi_peltigera_matrix_df$nostoc_otu <- factor(abmi_peltigera_matrix_df$nostoc_otu, levels = rev(colnames(peltigera_matrix_compound$matrix)))
-
-
-# Plot the matrix with compound topology
-# Start with the base plot
-peltigera_compound_plot <- ggplot(abmi_peltigera_matrix_df, aes(x = mycobiont_molecular_id, y = nostoc_otu, fill = interaction_frequency)) +
-            geom_tile(height = 0.8, width = 0.8) +
-            scale_fill_gradient(low = "white", high = "black",
-                guide = guide_colorbar(title = "Interaction frequency",
-                                    title.position = "top",
-                                    order = 1)) +
-            new_scale_fill() +
-            geom_tile(data = abmi_peltigera_matrix_df, 
-                aes(x = -0.3, y = nostoc_otu, fill = section), height = 1, width = 0.4) +
-            scale_fill_brewer(palette = "Set3",
-                guide = guide_legend(title = "Nostoc section",
-                                    title.position = "top",
-                                    order = 2)) +
-            new_scale_fill() +
-            geom_tile(data = abmi_peltigera_matrix_df, 
-                aes(x = mycobiont_molecular_id, y = -0.3, fill = peltigera_section), height = 0.4) +
-            scale_fill_brewer(palette = "Set2",
-                guide = guide_legend(title = "Peltigera section",
-                                    title.position = "top",
-                                    order = 3)) +
-            labs(x = "Peltigera species", y = "Nostoc OTU") +
-            theme_minimal() +
-            annotate("rect",
-                                xmin = 0,
-                                xmax = length(peltigera_x_partitions) + 1,
-                                ymin = 0,
-                                ymax = length(peltigera_y_partitions) + 1,
-                                color = "black", fill = NA, linetype = 1, size = 0.2) +
-            theme(axis.text = element_blank(),
-                panel.grid = element_blank(),
-                legend.position = "top",
-                legend.direction = "horizontal",
-                legend.key.height = unit(2, "mm"),
-                legend.key.width = unit(2, "mm"),
-                legend.text = element_text(size = 6),
-                legend.title = element_text(size = 6),
-                axis.title = element_text(size = 8))
-# Get unique partition values
-unique_partitions <- unique(c(peltigera_x_partitions, peltigera_y_partitions))
-# Loop over unique partition values
-for (i in unique_partitions) {
-    # Get indices for rows and columns
-    x_indices <- which(peltigera_x_partitions == i)
-    y_indices <- which(peltigera_y_partitions == i)       
-    # Add annotations for each partition
-    peltigera_compound_plot <- peltigera_compound_plot+ annotate("rect", 
-                                        xmin = min(x_indices) - 0.5, 
-                                        xmax = max(x_indices) + 0.5, 
-                                        ymin = min(y_indices) - 0.5, 
-                                        ymax = max(y_indices) + 0.5, 
-                                        color = "black", fill = NA, linetype = 1, size = 0.2) +
-                                    annotate("segment",
-                                        x = min(x_indices) - 0.5,
-                                        xend = min(x_indices) - 0.5,
-                                        y = 0,
-                                        yend = min(y_indices) - 0.5,
-                                        color = "gray70", linewidth = 0.2, linetype = 2) +
-                                    annotate("segment",
-                                        x = max(x_indices) + 0.5,
-                                        xend = max(x_indices) + 0.5,
-                                        y = max(y_indices) + 0.5,
-                                        yend = length(peltigera_y_partitions) + 1,
-                                        color = "gray70", linewidth = 0.2, linetype = 2) +
-                                    annotate("segment",
-                                        x = 0,
-                                        xend = min(x_indices) - 0.5,
-                                        y = min(y_indices) - 0.5,
-                                        yend = min(y_indices) - 0.5,
-                                        color = "gray70", linewidth = 0.2, linetype = 2) +
-                                    annotate("segment",
-                                        x = max(x_indices) + 0.5,
-                                        xend = length(peltigera_x_partitions) + 1,
-                                        y = min(y_indices) - 0.5,
-                                        yend = min(y_indices) - 0.5,
-                                        color = "gray70", linewidth = 0.2, linetype = 2)
-}
-# Save the plot
-ggsave(plot = peltigera_compound_plot, filename = "documents/plots/peltigera_compound.pdf", 
-    width = 17, height = 10, units = "cm")
-  
-
-#plotmatrix(peltigera_matrix_compound, binary = FALSE, border = TRUE, plot_labels = TRUE)
-
-
-
-
---
+++ scripts/ecology/plot_compound_network.R
@@ -0,0 +1,145 @@
+#!/usr/bin/env Rscript
+
+##### LOAD REQUIRED LIBRARIES AND CUSTOM FUNCTIONS #####
+
+# Load required libraries
+library(tidyverse)
+library(ggnewscale)
+
+
+##### PLOT THE NETWORKS HIGHLIGHTING COMPOUND STRUCTURE #####
+
+# Load ABMI data and interaction matrix
+abmi_data <- read_csv("data/tables/abmi_id_data.csv") 
+load("analyses/ecology/peltigera_interaction_matrix.RData")
+
+# Get module objects for the networks
+peltigera_modules <- bipartite::metaComputeModules(abmi_matrix_peltigera, N = 5, method = "Beckett")
+#full_modules <- bipartite::metaComputeModules(abmi_matrix_full, N = 5, method = "Beckett")
+
+# Get module partitions 
+peltigera_module_partitions <- bipartite::module2constraints(peltigera_modules)
+#full_module_partitions <- bipartite::module2constraints(full_module_partitions)
+
+# Get row and column partitions
+peltigera_row_partitions <- peltigera_module_partitions[1:nrow(abmi_matrix_peltigera)]
+peltigera_col_partitions <- peltigera_module_partitions[(nrow(abmi_matrix_peltigera)+1):(nrow(abmi_matrix_peltigera)+ncol(abmi_matrix_peltigera))]
+#full_row_partitions <- full_module_partitions[1:nrow(abmi_matrix_full)]
+#full_col_partitions <- full_module_partitions[(nrow(abmi_matrix_full)+1):(nrow(abmi_matrix_full)+ncol(abmi_matrix_full))]
+
+# Sort matrices in compound topology
+peltigera_matrix_compound <- bipartite::sortmatrix(abmi_matrix_peltigera, 
+    topology = "compound", sort_by = "weights", 
+    row_partitions = peltigera_row_partitions, col_partitions = peltigera_col_partitions,
+    mod_similarity = TRUE)
+
+# Get row and column partitions for the sorted matrix
+peltigera_x_partitions <- peltigera_matrix_compound$row_partitions
+peltigera_y_partitions <- rev(peltigera_matrix_compound$col_partitions)
+
+# Reshape matrix data for ggplot
+abmi_peltigera_matrix_df <- abmi_matrix_peltigera %>%
+    as.data.frame() %>%
+    rownames_to_column(var = "mycobiont_molecular_id") %>%
+    pivot_longer(cols = -mycobiont_molecular_id, names_to = "nostoc_otu", values_to = "interaction_frequency") %>%
+    left_join(distinct(abmi_data, mycobiont_molecular_id, peltigera_section), by = "mycobiont_molecular_id") %>%
+    left_join(distinct(abmi_data, nostoc_otu, section), by = "nostoc_otu") %>%
+    select(mycobiont_molecular_id, nostoc_otu, interaction_frequency, section,peltigera_section)
+
+# Set order for Nostoc section legend
+abmi_peltigera_matrix_df$section <- factor(abmi_peltigera_matrix_df$section, 
+                                        levels = c("section24", "section31", "section32", "section34", "section35", "section_36",
+                                            "section37", "section39", "section310", "section311", "section312"))
+
+# Convert Rows and Columns to factors with levels matching the orded of the sorted matrix
+abmi_peltigera_matrix_df$mycobiont_molecular_id <- factor(abmi_peltigera_matrix_df$mycobiont_molecular_id, levels = rownames(peltigera_matrix_compound$matrix))
+abmi_peltigera_matrix_df$nostoc_otu <- factor(abmi_peltigera_matrix_df$nostoc_otu, levels = rev(colnames(peltigera_matrix_compound$matrix)))
+
+
+# Plot the matrix with compound topology
+# Start with the base plot
+peltigera_compound_plot <- ggplot(abmi_peltigera_matrix_df, aes(x = mycobiont_molecular_id, y = nostoc_otu, fill = interaction_frequency)) +
+            geom_tile(height = 0.8, width = 0.8) +
+            scale_fill_gradient(low = "white", high = "black",
+                guide = guide_colorbar(title = "Interaction frequency",
+                                    title.position = "top",
+                                    order = 1)) +
+            new_scale_fill() +
+            geom_tile(data = abmi_peltigera_matrix_df, 
+                aes(x = -0.3, y = nostoc_otu, fill = section), height = 1, width = 0.4) +
+            scale_fill_brewer(palette = "Set3",
+                guide = guide_legend(title = "Nostoc section",
+                                    title.position = "top",
+                                    order = 2)) +
+            new_scale_fill() +
+            geom_tile(data = abmi_peltigera_matrix_df, 
+                aes(x = mycobiont_molecular_id, y = -0.3, fill = peltigera_section), height = 0.4) +
+            scale_fill_brewer(palette = "Set2",
+                guide = guide_legend(title = "Peltigera section",
+                                    title.position = "top",
+                                    order = 3)) +
+            labs(x = "Peltigera species", y = "Nostoc OTU") +
+            theme_minimal() +
+            annotate("rect",
+                                xmin = 0,
+                                xmax = length(peltigera_x_partitions) + 1,
+                                ymin = 0,
+                                ymax = length(peltigera_y_partitions) + 1,
+                                color = "black", fill = NA, linetype = 1, size = 0.2) +
+            theme(axis.text = element_blank(),
+                panel.grid = element_blank(),
+                legend.position = "top",
+                legend.direction = "horizontal",
+                legend.key.height = unit(2, "mm"),
+                legend.key.width = unit(2, "mm"),
+                legend.text = element_text(size = 6),
+                legend.title = element_text(size = 6),
+                axis.title = element_text(size = 8))
+
+# Get unique partition values
+unique_partitions <- unique(c(peltigera_x_partitions, peltigera_y_partitions))
+
+# Loop over unique partition values
+for (i in unique_partitions) {
+    # Get indices for rows and columns
+    x_indices <- which(peltigera_x_partitions == i)
+    y_indices <- which(peltigera_y_partitions == i)       
+    # Add annotations for each partition
+    peltigera_compound_plot <- peltigera_compound_plot+ annotate("rect", 
+                                        xmin = min(x_indices) - 0.5, 
+                                        xmax = max(x_indices) + 0.5, 
+                                        ymin = min(y_indices) - 0.5, 
+                                        ymax = max(y_indices) + 0.5, 
+                                        color = "black", fill = NA, linetype = 1, size = 0.2) +
+                                    annotate("segment",
+                                        x = min(x_indices) - 0.5,
+                                        xend = min(x_indices) - 0.5,
+                                        y = 0,
+                                        yend = min(y_indices) - 0.5,
+                                        color = "gray70", linewidth = 0.2, linetype = 2) +
+                                    annotate("segment",
+                                        x = max(x_indices) + 0.5,
+                                        xend = max(x_indices) + 0.5,
+                                        y = max(y_indices) + 0.5,
+                                        yend = length(peltigera_y_partitions) + 1,
+                                        color = "gray70", linewidth = 0.2, linetype = 2) +
+                                    annotate("segment",
+                                        x = 0,
+                                        xend = min(x_indices) - 0.5,
+                                        y = min(y_indices) - 0.5,
+                                        yend = min(y_indices) - 0.5,
+                                        color = "gray70", linewidth = 0.2, linetype = 2) +
+                                    annotate("segment",
+                                        x = max(x_indices) + 0.5,
+                                        xend = length(peltigera_x_partitions) + 1,
+                                        y = min(y_indices) - 0.5,
+                                        yend = min(y_indices) - 0.5,
+                                        color = "gray70", linewidth = 0.2, linetype = 2)
+}
+
+# Save the plot
+ggsave(plot = peltigera_compound_plot, filename = "documents/plots/peltigera_compound.pdf", 
+    width = 17, height = 10, units = "cm")
+  
+
+#plotmatrix(peltigera_matrix_compound, binary = FALSE, border = TRUE, plot_labels = TRUE)
