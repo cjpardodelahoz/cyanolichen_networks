0ab63cbb41093510f06c6ce831f8da137015001b
cjpardodelahoz <cjpardodelahoz@gmail.com>
2024-12-27 15:49:56 -0500

    updated shapes and module colors

--- scripts/ecology/plot_specif_vs_nsites.R
+++ scripts/ecology/plot_specif_vs_nsites.R
@@ -12,7 +12,9 @@ abmi_id_data <- read_csv("data/tables/abmi_id_data.csv") %>%
 
 # Load module assignments
 load("analyses/ecology/peltigera_module_assignments.RData")
-load("analyses/ecology/nostoc_module_assignments.RData")
+
+# Define custom colors for the modules
+module_colors <- c("1" = "#8a708a", "2" = "#7e937b", "3" = "#3277b0", "4" = "#be8551", "5" = "gray70", "6" = "gray20")
 
 # Summarize the table for mycobiont (Peltigera)
 peltigera_summary <- abmi_id_data %>%
@@ -24,7 +26,7 @@ peltigera_summary <- abmi_id_data %>%
   ) %>%
   rename(symbiont_id = mycobiont_molecular_id) %>%
   left_join(peltigera_module_assignments, by = c("symbiont_id" = "mycobiont_molecular_id")) %>%
-  mutate(color = ifelse(module %in% c(2, 3, 4), "#892b46", "gray50"))
+  mutate(color = module_colors[as.character(module)])
 
 # Summarize the table for nostoc
 nostoc_summary <- abmi_id_data %>%
@@ -36,7 +38,7 @@ nostoc_summary <- abmi_id_data %>%
   ) %>%
   rename(symbiont_id = nostoc_otu) %>%
   left_join(nostoc_module_assignments, by = c("symbiont_id" = "nostoc_otu")) %>%
-  mutate(color = ifelse(module %in% c(2, 3, 4), "#892b46", "gray50"))
+  mutate(color = module_colors[as.character(module)])
 
 ##### PLOT OCCURRENCE AND SPECIFICITY DATA #####
 
@@ -55,9 +57,9 @@ custom_theme <- theme(
 
 # Plot for Peltigera
 peltigera_plot <- ggplot(peltigera_summary, aes(x = n_sites, y = n_partners)) +
-  geom_point(aes(color = color)) +
+  geom_point(aes(color = color), shape = 17) +
   geom_smooth(method = "lm", se = FALSE, color = "gray50", linetype = "dashed", size = 0.5) +  # Add dashed trend line
-  scale_y_continuous(breaks = seq(0, 8, by = 2)) +  # Set y scale breaks
+  scale_y_continuous(breaks = seq(0, 10, by = 2)) +  # Set y scale breaks
   scale_color_identity() +  # Use the color column for point colors
   labs(x = "Number of sites",
        y = "Number of partners") +
